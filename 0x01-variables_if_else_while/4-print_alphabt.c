549d58b3c601f498f286daf3b77d084e529ace64
zan887 <yeabkalabreham887@gmail.com>
2024-07-18 16:47:55 +0300

    != e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/*
+ * main - Entry point
+ *
+ * Return - Always 0 (success)
+*/
+int main(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		if (a != 'e' && a != 'q')
+		{
+		putchar("%c", a);
+		a++;
+		}
+	}
+printf("\n"):
+return (0);
+}
