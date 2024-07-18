072f351950ecc827029fe495b16bd6a84da88c67
zan887 <yeabkalabreham887@gmail.com>
2024-07-18 14:50:52 +0300

    small letters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/*
+ *main - Entry point
+ *
+ * Return - Always 0 (success)
+ * */
+int main(void)
+{
+	char a = 'a';
+
+	while (a <='z')
+	{
+		printf("%c \n", a);
+		a++;
+	}
+return 0; 
+}
